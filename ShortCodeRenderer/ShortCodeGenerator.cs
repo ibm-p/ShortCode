c3792a354431a9ad0e23247f04b90942231cb413
İbrahim YUVACI <ltibm90@gmail.com>
2025-07-17 17:33:00 +0300

    Enhance short code functionality and UI updates
    - Modified `ShortCodeAttribute` to use `object` for `Value`.
    - Updated `ShortCodeAttributes` with new `GetObject` method.
    - Renamed `GetAttr` to `GetAttrStr` in `ShortCodeInfo.cs` and added new `GetAttr`.
    - Introduced `ShortCodeBuilder` for fluent short code creation.
    - Created `ShortCodeExtensions` for rendering and view data methods.
    - Added new Razor view `OnBasvuruFormu.cshtml` for user input.
    - Enhanced `CommonCache` with cache management methods.
    - Updated `ExtensionMethods.cs` for `ShortCodeBuilder` instance creation.
    - Integrated short code functionality in existing Razor views.
    - Updated project file to include new files.
    - Added new image `about-education-content.webp` for UI.
    - Made CSS improvements with new padding classes.
    - Updated `shortcodes.json` to include `OnBasvuruFormu`.
    - Modified `CacheUtil.cs` to use `CommonCache` for city data retrieval.

---
+++ ShortCodeRenderer/ShortCodeGenerator.cs
@@ -0,0 +1,45 @@
+﻿using ShortCodeRenderer.Common.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ShortCodeRenderer
+{
+    public class ShortCodeBuilder
+    {
+        ShortCodeContext _ctx;
+        ShortCodeInfo _info = new ShortCodeInfo();
+        public Dictionary<string, object> Variables { get; set; } = new Dictionary<string, object>();
+        public ShortCodeBuilder(ShortCodeContext ctx)
+        {
+            this._ctx = ctx;
+        }
+        public ShortCodeBuilder WithName(string name)
+        {
+            _info.Name = name;
+            return this;
+        }
+        public ShortCodeBuilder WithAttr(string name, object value)
+        {
+            _info.Attributes[name] = new ShortCodeAttribute()
+            {
+                Name = name,
+                Value = value
+            };
+            return this;
+        }
+        public ShortCodeBuilder WithContent(string content)
+        {
+            _info.Content = content;
+            return this;
+        }
+        public ShortCodeBuilder SetVariable<T>(string key, T value)
+        {
+            _ctx.SetVariable(key, value);
+            return this;
+        }
+        public string Render()
+        {
+            return _ctx.Render(_info);
+        }
+    }
+}
